4d529b8d9f9db88b6049e65fa81a8da2f1d6dcc9
Jeffrey <jeffreyjianyujie@gmail.com>
2023-10-12 18:43:04 +0800

    Add TransactionList model
    functions:
    - contains
    - add
    - setTransaction
    - remove
    etc..
    
    also added TransactionNotFoundException to handle transaction-
    related exceptions.

---
+++ src/main/java/seedu/address/model/transaction/Transaction.java
@@ -0,0 +1,13 @@
+package seedu.address.model.transaction;
+/**
+ *  Dummy class to support TransactionList.
+ */
+public class Transaction {
+
+    /**
+     * Dummy method to support TransactionList.
+     */
+    public boolean isSameTransaction(Transaction otherTransaction) {
+        return false;
+    }
+}
