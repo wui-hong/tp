2be890c3319377bd34b43b12228475bf30f89fdb
Nguyen Cao Duy <nguyen.cao.duy.0303@gmail.com>
2023-10-12 22:41:05 +0800

    Add Transaction model

---
+++ src/main/java/seedu/address/model/transaction/Transaction.java
@@ -0,0 +1,144 @@
+package seedu.address.model.transaction;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.commons.numbers.fraction.BigFraction;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.transaction.expense.Expense;
+
+/**
+ * Represents a Transaction in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Transaction {
+
+    // Data fields
+    private final Amount amount;
+    private final String description;
+    private final Person payee;
+    private final Set<Expense> expenses = new HashSet<>();
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Transaction(Amount amount, String description, Person payee, Set<Expense> expenses) {
+        requireAllNonNull(amount, description, payee);
+        this.amount = amount;
+        this.description = description;
+        this.payee = payee;
+        this.expenses.addAll(expenses);
+    }
+
+    public Amount getAmount() {
+        return amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Person getPayee() {
+        return payee;
+    }
+
+    /**
+     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
+     * if modification is attempted.
+     */
+    public Set<Expense> getExpenses() {
+        return Collections.unmodifiableSet(expenses);
+    }
+
+    /**
+     * Returns the portion of the transaction that the person has to pay.
+     *
+     * @param person the person to get the portion of
+     */
+    public BigFraction getPortion(Person person) {
+        BigFraction totalWeight = getTotalWeight();
+        return expenses.stream()
+            .filter(expense -> expense.getName().equals(person.getName()))
+            .map(expenses -> expenses.getWeight().value.multiply(this.amount.amount).divide(totalWeight))
+            .reduce(BigFraction.ZERO, BigFraction::add);
+    }
+
+    /**
+     * Returns a map of all the portions each person has to pay for this transaction.
+     */
+    public Map<Name, BigFraction> getAllPortions() {
+        BigFraction totalWeight = getTotalWeight();
+        return expenses.stream()
+            .collect(
+                Collectors.toMap(
+                    Expense::getName,
+                    expense -> expense.getWeight().value.multiply(this.amount.amount).divide(totalWeight)
+                )
+            );
+    }
+
+    /**
+     * Returns true if the person is involved in this transaction.
+     *
+     * @param person the person to check
+     */
+    public boolean isPersonInvolved(Person person) {
+        return expenses.stream()
+            .anyMatch(expense -> expense.getName().equals(person.getName()));
+    }
+
+    /**
+     * Returns true if both transactions have the same amount, description, payee and expenses.
+     * This defines a weaker notion of equality between two transactions.
+     */
+    public boolean isSameTransaction(Transaction otherTransaction) {
+        if (otherTransaction == this) {
+            return true;
+        }
+
+        return otherTransaction != null
+            && otherTransaction.getAmount().equals(getAmount())
+            && otherTransaction.getDescription().equals(getDescription())
+            && otherTransaction.getPayee().equals(getPayee())
+            && otherTransaction.getExpenses().equals(getExpenses());
+    }
+
+    /**
+     * Returns true if both transactions is the same object.
+     */
+    @Override
+    public boolean equals(Object other) {
+        return other == this;
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(amount, description, payee, expenses);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+            .add("amount", amount)
+            .add("description", description)
+            .add("payee", payee)
+            .add("expenses", expenses)
+            .toString();
+    }
+
+    private BigFraction getTotalWeight() {
+        return expenses.stream()
+            .map(expense -> expense.getWeight().value)
+            .reduce(BigFraction.ZERO, BigFraction::add);
+    }
+}
---
+++ src/test/java/seedu/address/testutil/TransactionBuilder.java
@@ -0,0 +1,80 @@
+package seedu.address.testutil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import seedu.address.model.person.Person;
+import seedu.address.model.transaction.Amount;
+import seedu.address.model.transaction.Transaction;
+import seedu.address.model.transaction.expense.Expense;
+
+/**
+ * A utility class to help with building Person objects.
+ */
+public class TransactionBuilder {
+
+    public static final String DEFAULT_AMOUNT = "12.34";
+    public static final String DEFAULT_DESCRIPTION = "Mala Xiang Guo at Clementi Mall on 12 Oct 2023";
+    public static final Person DEFAULT_PAYEE = TypicalPersons.ALICE;
+
+    private Amount amount;
+    private String description;
+    private Person payee;
+    private Set<Expense> expenses;
+
+    /**
+     * Creates a {@code TransactionBuilder} with the default details.
+     */
+    public TransactionBuilder() {
+        amount = new Amount(DEFAULT_AMOUNT);
+        description = DEFAULT_DESCRIPTION;
+        payee = DEFAULT_PAYEE;
+        expenses = new HashSet<>();
+    }
+
+    /**
+     * Initializes the TransactionBuilder with the data of {@code transactionToCopy}.
+     */
+    public TransactionBuilder(Transaction transactionToCopy) {
+        amount = transactionToCopy.getAmount();
+        description = transactionToCopy.getDescription();
+        payee = transactionToCopy.getPayee();
+        expenses = new HashSet<>(transactionToCopy.getExpenses());
+    }
+
+    /**
+     * Sets the {@code Amount} of the {@code Transaction} that we are building.
+     */
+    public TransactionBuilder withAmount(String amount) {
+        this.amount = new Amount(amount);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Description} of the {@code Transaction} that we are building.
+     */
+    public TransactionBuilder withDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    /**
+     * Sets the {@code Payee} of the {@code Transaction} that we are building.
+     */
+    public TransactionBuilder withPayee(Person payee) {
+        this.payee = payee;
+        return this;
+    }
+
+    /**
+     * Sets the {@code Expenses} of the {@code Transaction} that we are building.
+     */
+    public TransactionBuilder withExpenses(Set<Expense> expenses) {
+        this.expenses = expenses;
+        return this;
+    }
+
+    public Transaction build() {
+        return new Transaction(amount, description, payee, expenses);
+    }
+}
