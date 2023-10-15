3227c178cf4e304433a3159476d89c7e8dc40b5b
nathanielcalimag <e0941371@u.nus.edu>
2023-10-15 19:01:13 +0800

    Add unit tests for settle command

--- src/test/java/seedu/address/logic/commands/SettlePersonCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SettlePersonCommandTest.java
@@ -1,8 +1,45 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+
 /**
- * Contains integration tests (interaction with the Model) and unit tests for
- * {@code SettlePersonCommand}.
+ * Contains unit tests for {@code SettlePersonCommand}.
  */
 public class SettlePersonCommandTest {
+    @Test
+    public void equals() {
+        SettlePersonCommand settleFirstCommand = new SettlePersonCommand(INDEX_FIRST_PERSON);
+        SettlePersonCommand settleSecondCommand = new SettlePersonCommand(INDEX_SECOND_PERSON);
+
+        // same object -> returns true
+        assertEquals(settleFirstCommand, settleFirstCommand);
+
+        // same values -> returns true
+        SettlePersonCommand settleFirstCommandCopy = new SettlePersonCommand(INDEX_FIRST_PERSON);
+        assertEquals(settleFirstCommand, settleFirstCommandCopy);
+
+        // different types -> returns false
+        assertNotEquals(1, settleFirstCommand);
+
+        // null -> returns false
+        assertNotEquals(null, settleFirstCommand);
+
+        // different person -> returns false
+        assertNotEquals(settleFirstCommand, settleSecondCommand);
+    }
+
+    @Test
+    public void toStringMethod() {
+        Index targetIndex = Index.fromOneBased(1);
+        SettlePersonCommand settleCommand = new SettlePersonCommand(targetIndex);
+        String expected = SettlePersonCommand.class.getCanonicalName() + "{targetIndex=" + targetIndex + "}";
+        assertEquals(expected, settleCommand.toString());
+    }
 }
