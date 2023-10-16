a9aed86c4bb4264edfedd1f906efe359aee14e6f
Nguyen Cao Duy <caoduy73@gmail.com>
2023-10-16 09:05:36 +0000

    Update src/main/java/seedu/address/logic/parser/DeleteTransactionCommandParser.java
    Co-authored-by: Nathaniel Calimag <89729098+nathanielcalimag@users.noreply.github.com>

--- src/main/java/seedu/address/logic/parser/DeleteTransactionCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteTransactionCommandParser.java
@@ -7,7 +7,7 @@ import seedu.address.logic.commands.DeleteTransactionCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new DeleteCommand object
+ * Parses input arguments and creates a new DeleteCommand object.
  */
 public class DeleteTransactionCommandParser implements Parser<DeleteTransactionCommand> {
 
