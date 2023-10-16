4ee4dfc1ef61d4ee0c1c4579d568d3a8c108ac6f
jedkohjk <jedkoh2001@gmail.com>
2023-10-17 04:53:16 +0800

    Add tests

--- src/test/java/seedu/address/testutil/AddressBookBuilder.java
+++ src/test/java/seedu/address/testutil/AddressBookBuilder.java
@@ -2,6 +2,8 @@ package seedu.address.testutil;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.person.Person;
+import seedu.address.model.transaction.Transaction;
+
 
 /**
  * A utility class to help with building Addressbook objects.
@@ -25,6 +27,14 @@ public class AddressBookBuilder {
      */
     public AddressBookBuilder withPerson(Person person) {
         addressBook.addPerson(person);
+        return this;
+    }
+
+    /**
+     * Adds a new {@code Transaction} to the {@code AddressBook} that we are building.
+     */
+    public AddressBookBuilder withTransaction(Transaction transaction) {
+        addressBook.addTransaction(transaction);
         return this;
     }
 
