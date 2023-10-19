2073d4e6d3e251f96b6d8bb10c2eaa0f5deca4d3
jedkohjk <jedkoh2001@gmail.com>
2023-10-19 19:39:46 +0800

    Resolve merge conflicts

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -13,5 +13,6 @@ public class CliSyntax {
     public static final Prefix PREFIX_TAG = new Prefix("t=");
     public static final Prefix PREFIX_COST = new Prefix("c=");
     public static final Prefix PREFIX_DESCRIPTION = new Prefix("d=");
+    public static final Prefix PREFIX_WEIGHT = new Prefix("w=");
 
 }
