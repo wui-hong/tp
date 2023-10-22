562fdf7f66e237650237954470ab9d77ec3e75c1
jedkohjk <jedkoh2001@gmail.com>
2023-10-23 00:33:09 +0800

    Remove print statement

--- src/test/java/seedu/address/model/transaction/UniqueTransactionListTest.java
+++ src/test/java/seedu/address/model/transaction/UniqueTransactionListTest.java
@@ -154,7 +154,6 @@ class UniqueTransactionListTest {
         transactionList.add(new TransactionBuilder().withAmount("3").withPayeeName(Name.SELF.fullName)
             .withExpenses(Set.of(ALICE_EXPENSE, BENSON_EXPENSE)).build());
         assertTrue(transactionList.getBalance(ALICE.getName()).equals(BigFraction.ONE));
-        System.out.println(transactionList.getBalance(BENSON.getName()));
         assertTrue(UniqueTransactionList.getBalance(BENSON.getName(),
                 transactionList.asUnmodifiableObservableList()).equals(BigFraction.ONE.add(BigFraction.ONE)));
     }
