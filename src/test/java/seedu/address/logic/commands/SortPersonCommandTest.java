701cf1b4d8df05fd8859da2ce6e554ad9cc4e5ad
jedkohjk <jedkoh2001@gmail.com>
2023-10-26 22:57:13 +0800

    Add tests

--- src/test/java/seedu/address/logic/commands/SortPersonCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortPersonCommandTest.java
@@ -2,11 +2,34 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static seedu.address.logic.commands.CommandTestUtil.assertTransactionCommandSuccess;
+import static seedu.address.testutil.TypicalAddressBook.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
 
 public class SortPersonCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute() {
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        SortPersonCommand cmd = new SortPersonCommand(false);
+        String expectedMessage = String.format(SortPersonCommand.MESSAGE_SORT_PERSON_SUCCESS, "ascending");
+        expectedModel.sortPersonAscending();
+        assertTransactionCommandSuccess(cmd, model, expectedMessage, expectedModel);
+        cmd = new SortPersonCommand(true);
+        expectedMessage = String.format(SortPersonCommand.MESSAGE_SORT_PERSON_SUCCESS, "descending");
+        expectedModel.sortPersonDescending();
+        assertTransactionCommandSuccess(cmd, model, expectedMessage, expectedModel);
+    }
+
     @Test
     public void equals() {
         SortPersonCommand sortDescCommand = new SortPersonCommand(true);
