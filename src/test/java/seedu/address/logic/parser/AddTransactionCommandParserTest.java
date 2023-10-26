5adeb72d390ffeb67a3e5c63e490068c20a7d43a
jedkohjk <jedkoh2001@gmail.com>
2023-10-26 22:39:49 +0800

    Add tests

--- src/test/java/seedu/address/logic/parser/AddTransactionCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddTransactionCommandParserTest.java
@@ -3,6 +3,7 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.COST_DESC_LUNCH;
 import static seedu.address.logic.commands.CommandTestUtil.DESCRIPTION_DESC_LUNCH;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_TIMESTAMP;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_COST_LUNCH;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_DESCRIPTION_LUNCH;
@@ -43,6 +44,26 @@ public class AddTransactionCommandParserTest {
         // missing weight prefix
         assertParseFailure(parser, DESCRIPTION_DESC_LUNCH + NAME_DESC_AMY + COST_DESC_LUNCH
                 + " " + PREFIX_NAME + Name.SELF + " " + VALID_WEIGHT_ONE, expectedMessage);
+
+        // missing description prefix
+        assertParseFailure(parser, " " + VALID_DESCRIPTION_LUNCH + NAME_DESC_AMY + COST_DESC_LUNCH
+                + DESC_TIMESTAMP + " " + PREFIX_NAME + Name.SELF + WEIGHT_DESC_ONE, expectedMessage);
+
+        // missing name prefix
+        assertParseFailure(parser, DESCRIPTION_DESC_LUNCH + " " + VALID_NAME_AMY + COST_DESC_LUNCH
+                + " " + PREFIX_NAME + Name.SELF + WEIGHT_DESC_ONE, expectedMessage);
+
+        // missing cost prefix
+        assertParseFailure(parser, DESCRIPTION_DESC_LUNCH + NAME_DESC_AMY + " " + VALID_COST_LUNCH
+                + DESC_TIMESTAMP + " " + PREFIX_NAME + Name.SELF + WEIGHT_DESC_ONE, expectedMessage);
+
+        // missing name prefix
+        assertParseFailure(parser, DESCRIPTION_DESC_LUNCH + NAME_DESC_AMY + COST_DESC_LUNCH
+                + DESC_TIMESTAMP + " " + Name.SELF + WEIGHT_DESC_ONE, expectedMessage);
+
+        // missing weight prefix
+        assertParseFailure(parser, DESCRIPTION_DESC_LUNCH + NAME_DESC_AMY + COST_DESC_LUNCH
+                + DESC_TIMESTAMP + " " + PREFIX_NAME + Name.SELF + " " + VALID_WEIGHT_ONE, expectedMessage);
     }
 
 }
